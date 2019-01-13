15596255225cc62b690fa4fa3752025549fdd65d
hannahstwinter <hstwinter@gmail.com>
2019-01-13 15:11:47 -0500

    update nav button classes for consistency

--- app/assets/javascripts/navigation/nav_bar.jsx
+++ app/assets/javascripts/navigation/nav_bar.jsx
@@ -18,8 +18,8 @@ NavBarDescription.render = function () {
 
   return (
     <div id="nav-bar">
-      <NavMenuButton title="HW" className="home-nav" route="/#" />
-      <NavMenuButton title="Resume" className="resume-nav" route="/resume" />
+      <NavMenuButton title="HW" className="home" route="/#" />
+      <NavMenuButton title="Resume" className="resume" route="/resume" />
       <NavMenuButton title="Blog" className="blog" menu={ <BlogMenu /> } { ...sharedMenuProps } />
       <NavMenuButton title="Projects" className="projects" menu={ <ProjectMenu /> } { ...sharedMenuProps } />
       <NavMenuButton title="Art" className="art" menu={ <ArtMenu /> } { ...sharedMenuProps } />
